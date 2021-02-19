2880e66680352f9046c1ef93e860687328722309
andresles13 <andreslesmescu09@gmaill.com>
2021-02-18 23:25:48 -0500

    holi

--- src/screen/Home.js
+++ src/screen/Home.js
@@ -4,7 +4,7 @@ import { View, Text } from "react-native";
 const Home = () => {
   return (
     <View>
-      <Text>Esta es la pantalla del inicio</Text>
+      <Text>Esta es la pantalla del inici :)o|-</Text>
     </View>
   );
 };
